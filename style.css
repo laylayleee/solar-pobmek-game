48fb426de6811c14f551484ac5d8d92156e3b582
laylayleee <aishaangelhen@gmail.com>
2026-01-31 01:10:50 +0300

    Add initial styles for body and canvas elements

---
+++ style.css
@@ -0,0 +1,12 @@
+body {
+  text-align: center;
+  background-color: #111;
+  color: white;
+  font-family: Arial, sans-serif;
+}
+
+canvas {
+  background-color: #222;
+  border: 3px solid white;
+  margin-top: 10px;
+}
